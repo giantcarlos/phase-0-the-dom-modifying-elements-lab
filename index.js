344385ef8823a1d1db625db2f873c268d06dd9d0
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-01-28 14:22:51 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const element = document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.innerHTML="Gian is the champion";
